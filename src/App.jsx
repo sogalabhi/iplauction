4eef49664e6ca1c3836cc6cce708eb5333f8d924
sogalabhi <abhijithsogal@gmail.com>
2025-01-15 22:53:58 +0530

    fetch player data from Supabase and implement fetching functions for expensive and previous players

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { fetchSupabaseData } from './utils/getFromRemote'
+import { fetchExpensivePlayer } from './utils/expensivePlayer';
+import { fetchPrevPlayer } from './utils/previousPlayer';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [players, setplayers] = useState([]);
+
+  const fetchAllPlayers = async () => {
+    var res = await fetchSupabaseData('CricketPlayers');
+    setplayers(res);
+  }
+  const fetchAllTeams = async () => {
+    var res = await fetchSupabaseData('Teams');
+    setplayers(res);
+  }
+  const getExpensivePlayer = async () => {
+    var res = await fetchExpensivePlayer();
+    setplayers(res);
+  }
+  const getPrevPlayer = async () => {
+    var res = await fetchPrevPlayer();
+    setplayers(res);
+    console.log(res);
+  }
+
+  useEffect(() => {
+    fetchAllPlayers();
+    fetchAllTeams();
+    getExpensivePlayer();
+    getPrevPlayer();
+  }, [])
 
   return (
     <>
-      
+
     </>
   )
 }
